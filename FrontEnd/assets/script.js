c3984460df4fb34d0a65359b4969d687da75b426
CedricC31 <caumeilcedric@yahoo.fr>
2023-06-30 18:39:52 +0200

    Etape 1.2

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -3,31 +3,50 @@
 // ********* CONSTANTES *********
 
 const gallery = document.querySelector('.gallery');
+const filterButtons = document.querySelectorAll('.filter');
 
 // ********* FONCTIONS *********
 
 /**
  * Récupère les travaux à partir de l'API spécifiée et les ajoute à la galerie.
  */
-function fetchWorks() {
+function fetchWorks(category) {
   fetch('http://localhost:5678/api/works')
-  .then(res => res.json())
-  .then(data => {
-    data.forEach(project => {
-      const figure = document.createElement('figure');
-      const img = document.createElement('img');
-      const figcaption = document.createElement('figcaption');
+    .then(res => res.json())
+    .then(data => {
+      gallery.innerHTML = ''; // Efface le contenu précédent de la galerie
 
-      img.src = project.imageUrl;
-      img.setAttribute('alt', project.title);
-      figcaption.textContent = project.title;
+      data.forEach(project => {
+        if (category === 'all' || category === project.category.name.toLowerCase() || !category) {
+          const figure = document.createElement('figure');
+          const img = document.createElement('img');
+          const figcaption = document.createElement('figcaption');
 
-      figure.appendChild(img);
-      figure.appendChild(figcaption);
-      gallery.appendChild(figure);
+          img.src = project.imageUrl;
+          img.setAttribute('alt', project.title);
+          figcaption.textContent = project.title;
+
+          figure.appendChild(img);
+          figure.appendChild(figcaption);
+          gallery.appendChild(figure);
+        }
+      });
+    })
+    .catch(error => {
+      console.error('Une erreur s\'est produite lors de la récupération des projets:', error);
     });
-  });
 }
+
+filterButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    filterButtons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+
+    const category = button.getAttribute('data-category');
+    
+    fetchWorks(category);
+  });
+});
 
 // ********* MAIN *********
 
